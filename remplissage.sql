6cdf46dfd4b30ed87cd45dd4fcaa0a249ce29df8
Samia BENNI <sbenni@deule.net>
2017-11-24 16:26:40 +0100

    base de donnée

---
+++ remplissage.sql
@@ -0,0 +1,21 @@
+INSERT INTO categorie VALUES('Action', 'Met en scène une succession de scènes spectaculaires souvent stéréotypées');
+INSERT INTO categorie VALUES('Animation', 'Utilise des images de synthèse, se destinant souvent aux enfants et aux familles');
+INSERT INTO categorie VALUES('Aventure', 'Se caractérise par la présence d un héros fictif ou non, tirant son statut du mythe qu il inspire, l action particulière qui s y déroule, ou l emploi de décors particuliers');
+INSERT INTO categorie VALUES('Biopic', 'Oeuvre de fiction centrée sur la description biographique d une personne ayant réellement existé.');
+INSERT INTO categorie VALUES('Bollywood', 'Industrie du cinméa musical indien, produit en langue hindi');
+INSERT INTO categorie VALUES('Comédie', 'A pour but d amuser les spectateurs à travers des situations, des dialogues ou des gestes à même de faire rire.');
+INSERT INTO categorie VALUES('Documentaire', 'Film à caractère didactique ou informatif qui vise principalement à restituer les apparences de la réalité');
+INSERT INTO categorie VALUES('Drame', 'Traite de situations généralement non-épiques dans un contexte sérieux, sur un ton plus susceptible d inspirer la tristesse que le rire.');
+INSERT INTO categorie VALUES('Familial', 'Film à regarder en famille, au ton léger, sans vulgarité');
+INSERT INTO categorie VALUES('Fantastique', 'Fait appel au surnaturel, à l horreur, à l insolite ou aux monstres. L’intrigue se fonde sur des éléments irrationnels, ou irréalistes.');
+INSERT INTO categorie VALUES('Guerre', 'Traitant le thème de la guerre, en s attardant généralement sur l aspect naval, aérien, ou terrestre des conflits.');
+INSERT INTO categorie VALUES('Horreur', 'A pour objectif est de créer un sentiment de peur, de répulsion ou d angoisse chez le spectateur.');
+INSERT INTO categorie VALUES('Musical', 'Se caractérise par son alternance entre scènes dialoguées et instants musicaux,  de la musique, des chansons ou de la danse.');
+INSERT INTO categorie VALUES('Policier', 'Met en scène le milieu du crime ou de la police, souvent par la résolution d une enquête par un policier ou un détective.');
+INSERT INTO categorie VALUES('Romance', 'Portant sur une histoire d amour ou d aventure amoureuse, mettant en avant la passion, les émotions et l engagement affectif des personnages principaux.');
+INSERT INTO categorie VALUES('Science-fiction', 'Raconte une fiction reposant sur des progrès scientifiques et techniques obtenus dans un futur plus ou moins lointain, des voyages inter-planétaire, mettant en scène des robots et des technologies futuristes.');
+INSERT INTO categorie VALUES('Thriller', 'Provoque chez le spectateur une excitation ou une appréhension et le tient en haleine jusqu au dénouement de l intrigue.');
+INSERT INTO categorie VALUES('Western', 'Se déroule généralement en Amérique du Nord lors de la conquête de l Ouest dans les dernières décennies du XIXème siècle.');
+
+
+
