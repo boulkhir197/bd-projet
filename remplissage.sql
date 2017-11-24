4f07e4f3703739abd5c9f28398df66adecfa4268
Samia BENNI <sbenni@deule.net>
2017-11-24 17:56:57 +0100

    base de donnée

--- remplissage.sql
+++ remplissage.sql
@@ -1,3 +1,5 @@
+/*Remplissage table catégorie */
+
 INSERT INTO categorie VALUES('Action', 'Met en scène une succession de scènes spectaculaires souvent stéréotypées');
 INSERT INTO categorie VALUES('Animation', 'Utilise des images de synthèse, se destinant souvent aux enfants et aux familles');
 INSERT INTO categorie VALUES('Aventure', 'Se caractérise par la présence d un héros fictif ou non, tirant son statut du mythe qu il inspire, l action particulière qui s y déroule, ou l emploi de décors particuliers');
@@ -16,6 +18,106 @@ INSERT INTO categorie VALUES('Romance', 'Portant sur une histoire d amour ou d a
 INSERT INTO categorie VALUES('Science-fiction', 'Raconte une fiction reposant sur des progrès scientifiques et techniques obtenus dans un futur plus ou moins lointain, des voyages inter-planétaire, mettant en scène des robots et des technologies futuristes.');
 INSERT INTO categorie VALUES('Thriller', 'Provoque chez le spectateur une excitation ou une appréhension et le tient en haleine jusqu au dénouement de l intrigue.');
 INSERT INTO categorie VALUES('Western', 'Se déroule généralement en Amérique du Nord lors de la conquête de l Ouest dans les dernières décennies du XIXème siècle.');
+
+
+
+
+/* Remplissage table Série*/
+
+
+INSERT INTO serie VALUES('S00001', 'Star Wars');
+INSERT INTO serie VALUES('S00002', 'Le Seigneur des anneaux');
+INSERT INTO serie VALUES('S00003', 'Harry Potter');
+INSERT INTO serie VALUES('S00004', 'Men in Black');
+INSERT INTO serie VALUES('S00005', 'X-men');
+INSERT INTO serie VALUES('S00006', 'Pirate des Caraïbes');
+INSERT INTO serie VALUES('S00007', 'Hunger Games');
+INSERT INTO serie VALUES('S00008', 'Matrix');
+INSERT INTO serie VALUES('S00009', 'Taken');
+INSERT INTO serie VALUES('S00010', 'Saw');
+INSERT INTO serie VALUES('S00011', 'Indiana Jones');
+INSERT INTO serie VALUES('S00012', 'Twilight');
+INSERT INTO serie VALUES('S00013', 'Resident Evil');
+INSERT INTO serie VALUES('S00014', 'Rambo');
+INSERT INTO serie VALUES('S00015', 'Rocky');
+INSERT INTO serie VALUES('S00016', 'Terminator');
+INSERT INTO serie VALUES('S00017', 'James Bond');
+INSERT INTO serie VALUES('S00018', 'Sex and the city');
+INSERT INTO serie VALUES('S00019', 'Le parrain');
+INSERT INTO serie VALUES('S00020', 'Star Trek');
+
+
+/* Remplissage table Format */
+
+INSERT INTO format VALUES('xvid');
+INSERT INTO format VALUES('avi');
+INSERT INTO format VALUES('mov');
+INSERT INTO format VALUES('dvix');
+INSERT INTO format VALUES('mkv');
+INSERT INTO format VALUES('flv');
+INSERT INTO format VALUES('wmv');
+INSERT INTO format VALUES('mp4');
+
+
+
+
+/* Remplissage table acteur */
+
+INSERT INTO acteur VALUES('RADDAN89', 'Radcliffe', 'Daniel');
+INSERT INTO acteur VALUES('WATEMM90', 'Watson', 'Emma');
+INSERT INTO acteur VALUES('WOOELI81', 'Wood', 'Elijah');
+INSERT INTO acteur VALUES('PORNAT81', 'Portman', 'Natalie');
+INSERT INTO acteur VALUES('FISCAR56', 'Fisher', 'Carrie');
+INSERT INTO acteur VALUES('FORHAR42', 'Ford', 'Harrison');
+INSERT INTO acteur VALUES('HAMMAR51', 'Hamill', 'Mark');
+INSERT INTO acteur VALUES('DRIADA83', 'Driver', 'Adam');
+INSERT INTO acteur VALUES('SMIWIL68', 'Smith',  'Will');
+INSERT INTO acteur VALUES('JONTOM46', 'Jones', 'Tommy Lee');
+INSERT INTO acteur VALUES('JACHUG68', 'Jackman', 'Hugh');
+INSERT INTO acteur VALUES('REYRAY76', 'Reynolds', 'Rayan');
+INSERT INTO acteur VALUES('SCHLIE67', 'Schreiber', 'Liev');
+INSERT INTO acteur VALUES('DEPJON63', 'Depp', 'Johnny');
+INSERT INTO acteur VALUES('KNIKEI85', 'Knightley', 'Keira');
+INSERT INTO acteur VALUES('BLOORL77', 'Bloom', 'Orlando');
+INSERT INTO acteur VALUES('LAWJEN90', 'Lawrence', 'Jennifer');
+INSERT INTO acteur VALUES('HUTJOS92', 'Hutcherson', 'Josh');
+INSERT INTO acteur VALUES('HEMLIA90', 'Hemsworth', 'Liam');
+INSERT INTO acteur VALUES('HARWOO61', 'Harrelson', 'Woody');
+INSERT INTO acteur VALUES('REEKEA64', 'Reeves', 'Keanu');
+INSERT INTO acteur VALUES('FISLAU61', 'Fishburne', 'Laurence');
+INSERT INTO acteur VALUES('MOSCAR67', 'Moss', 'Carrie-Anne');
+INSERT INTO acteur VALUES('NESLIA52', 'Nesson', 'Liam');
+INSERT INTO acteur VALUES('WHALEI77', 'Whannell', 'Leigh');
+INSERT INTO acteur VALUES('STEKRI90', 'Sterwart', 'Kristen');
+INSERT INTO acteur VALUES('PATROB86', 'Pattinson', 'Robert');
+INSERT INTO acteur VALUES('LAUTAY82', 'Lautner', 'Taylor');
+INSERT INTO acteur VALUES('JOVMIL75', 'Jovovich', 'Milla');
+INSERT INTO acteur VALUES('STASYL46', 'Stallone', 'Sylvester');
+INSERT INTO acteur VALUES('SCHARN47', 'Schwarzenegger', 'Arnold');
+INSERT INTO acteur VALUES('CONSEA30', 'Connery', 'Sean');
+INSERT INTO acteur VALUES('CRADAN68', 'Craig', 'Daniel');
+INSERT INTO acteur VALUES('PARSAR65', 'Parker', 'Sarah Jessica');
+INSERT INTO acteur VALUES('PACALL40', 'Pacino', 'Al');
+INSERT INTO acteur VALUES('BRAMAR24', 'Brando', 'Marlon');
+INSERT INTO acteur VALUES('NIMLEO31', 'Nimmoy', 'Leonard');
+INSERT INTO acteur VALUES('QUIZAC77', 'Quinto', 'Zachary');
+INSERT INTO acteur VALUES('PITBRA63', 'Pitt', 'Brad');
+INSERT INTO acteur VALUES('JOHSCA84', 'Johansson', 'Scarlett');
+INSERT INTO acteur VALUES('DICLEO74', 'Di Caprio', 'Leonardo');
+INSERT INTO acteur VALUES('DENROB43', 'De Niro', 'Robert');
+INSERT INTO acteur VALUES('STRMER49', 'Streep', 'Meryl');
+INSERT INTO acteur VALUES('WINKAT75', 'Winslet', 'Kate');
+INSERT INTO acteur VALUES('ROBJUL67', 'Roberts', 'Julia');
+
+
+
+
+/* Table film */
+
+
+INSERT 
+
+
 
 
 
