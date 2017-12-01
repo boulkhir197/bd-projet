d088c26135a215aa419d2f97c784286ffd8773db
Samia BENNI <sbenni@deule.net>
2017-12-01 14:19:52 +0100

    index.php

---
+++ php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title> Up Heaven - Le paradis du téléchargement </title>
+</head>
+
+<body>
+	
+	<div>
+	
+	
+	</div>
+	
+	
+	<div>
+	
+	
+	<div/>
+	
+	
+	
+	<ul>
+	</ul>
+
+</body>
+</html>
