b4940ce109b3644f36a3a929844930ead2c6d968
Mohamed Boulkhir <mohamed.boulkhir@etudiant.univ-lille1.fr>
2017-12-09 20:20:41 +0100

    array

--- lib/categorie.php
+++ lib/categorie.php
@@ -5,6 +5,6 @@ $base = Database::getConnection();
 $query = "SELECT * FROM Categorie ORDER BY label";
 $resquery = pg_query($base, $query); 
 while ($line = pg_fetch_assoc($resquery)) {
-    $categorie[$line['label']]=>$line['description'];
+    $categorie[$line['label']]=$line['description'];
 }
 ?>
